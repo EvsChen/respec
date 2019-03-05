b023f9492d164f9798d117ca73451ab5d30eff01
EvsChen <chensy1996@sjtu.edu.cn>
2019-03-06 00:59:47 +0800

    sync

--- src/core/mdn-annotation.js
+++ src/core/mdn-annotation.js
@@ -1,5 +1,5 @@
 import { fetchAndCache } from "./utils";
-import hyperHTML from "hyperhtml";
+import hyperHTML, { hyper } from "hyperhtml";
 import mdnCss from "text!../../assets/mdn-annotation.css";
 
 export const name = "core/mdn-annoatation";
@@ -8,11 +8,59 @@ const SPEC_MAP_URL =
   "https://raw.githubusercontent.com/w3c/mdn-spec-links/master/SPECMAP.json";
 const JSON_BASE = "https://w3c.github.io/mdn-spec-links/";
 const MDN_URL_BASE = "https://developer.mozilla.org/en-US/docs/Web/";
+const MDN_BROWSERS = {
+  // The browser IDs here must match the ones in the imported JSON data.
+  // See the list of browser IDs at https://goo.gl/iDacWP.
+  chrome: "Chrome",
+  chrome_android: "Chrome Android",
+  edge: "Edge",
+  edge_mobile: "Edge Mobile",
+  firefox: "Firefox",
+  firefox_android: "Firefox Android",
+  ie: "Internet Explorer",
+  // nodejs: "Node.js", // no data for features in HTML
+  opera: "Opera",
+  opera_android: "Opera Android",
+  // qq_android: "QQ Browser", // not enough data for features in HTML
+  safari: "Safari",
+  safari_ios: "Safari iOS",
+  samsunginternet_android: "Samsung Internet",
+  // uc_android: "UC browser", // not enough data for features in HTML
+  // uc_chinese_android: "Chinese UC Browser", // not enough data for features in HTML
+  webview_android: "WebView Android",
+};
 
 async function fetchAndCacheJson(url, maxAge) {
   if (!url) return {};
   const request = new Request(url);
   return await fetchAndCache(request, maxAge).then(r => r.json());
+}
+
+function addMDNBrowserRow(supportTable, browserId, yesNoUnknown, version) {
+  const displayStatus = yesNoUnknown === "Unknown" ? "?" : yesNoUnknown;
+  const browserRow = hyperHTML`
+    <span class="${browserId} ${yesNoUnknown.toLowerCase()}">
+      <span>${MDN_BROWSERS[browserId]}</span>
+      <span>${version ? version : displayStatus}</span>
+    </span>`;
+  supportTable.appendChild(browserRow);
+}
+
+function processBrowserData(browserId, versionData, supportTable) {
+  if (versionData.version_removed) {
+    addMDNBrowserRow(supportTable, browserId, "No", "");
+    return;
+  }
+  const versionAdded = versionData.version_added;
+  if (!versionAdded) {
+    addMDNBrowserRow(supportTable, browserId, "Unknown", "");
+    return;
+  }
+  if (typeof versionAdded === "boolean") {
+    addMDNBrowserRow(supportTable, browserId, versionAdded ? "Yes" : "No", "");
+  } else {
+    addMDNBrowserRow(supportTable, browserId, "Yes", `${versionAdded}+`);
+  }
 }
 
 export async function run(conf) {
@@ -71,7 +119,7 @@ export async function run(conf) {
         .map(spec => {
           const mdnDiv = document.createElement("div");
           mdnDiv.innerHTML += `<b>MDN </b>`;
-          const { slug, summary } = spec;
+          const { slug, summary, support } = spec;
           const mdnSubPath = slug.slice(slug.indexOf("/") + 1);
           mdnDiv.innerHTML += `
             <details>
@@ -80,6 +128,31 @@ export async function run(conf) {
               </summary>
             </details>
           `;
+          if (!support) {
+            mdnDiv.appendChild(hyperHTML`
+              <p class="nosupportdata">No support data.</p>
+            `);
+          } else {
+            const supportTable = hyperHTML`<p class="mdnsupport"></p>`;
+            mdnDiv.appendChild(supportTable);
+            for (const browserId of MDN_BROWSERS) {
+              if (!support[browserId]) {
+                addMDNBrowserRow(supportTable, browserId, "Unknown", "");
+              } else {
+                if (Array.isArray(support[browserId])) {
+                  support[browserId].forEach(b => {
+                    processBrowserData(browserId, b, supportTable);
+                  });
+                } else {
+                  processBrowserData(
+                    browserId,
+                    support[browserId],
+                    supportTable
+                  );
+                }
+              }
+            }
+          }
           return mdnDiv;
         })
         .forEach(mdnDiv => mdnBox.appendChild(mdnDiv));
